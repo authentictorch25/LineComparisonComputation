1aa417a28d94cde0918ac16a36a4477b3b12bc6c
Akash Pratap Singh <akashpratap2507@gmail.com>
2020-10-03 18:25:55 +0530

    [Akash] Added Program to read coordinates

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
 
 namespace LineComparison
 {
@@ -7,6 +10,26 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program ");
+            int length = 0;
+            Console.WriteLine("Enter the first coordinate");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Second Coordinate");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("First Coordinate : " + x1 + "," +y1);
+            Console.WriteLine("Second Coordinate : " +x2 + "," + y2);
+
+            Console.ReadKey();
+
+
+
+
+
+
         }
+
     }
 }
